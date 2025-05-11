b07cfcc3dcbb2b9a43d2cc29630c16f3a236ad87
Somdeep Dutta <Somdeep Dutta@LAPTOP-G8JJL783>
2025-05-11 17:15:27 +0530

    Casting second push

---
+++ Casting/src/pack3/Explicit.java
@@ -0,0 +1,15 @@
+package pack3;
+
+public class Explicit 
+{
+		public static void main(String[] args) 
+		{
+			float f = 20.9347F;
+			int a = (int)f;
+			System.out.println(a);
+			
+		}
+
+	
+
+}
